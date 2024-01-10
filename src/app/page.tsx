cd675684de3e2d4d8320ca836639f1e75914519d
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-01-09 23:59:26 -0600

    feat(Home Page): add image file upload preview (Need to update when finish the project)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,6 +131,22 @@ export default function Home() {
           </li>
         </ol>
 
+        {/* Image Upload */}
+        <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <Image 
+                    src='/file-upload-preview.jpg'
+                    alt='file upload preview'
+                    width={1419}
+                    height={732}
+                    quality={100}
+                    className='rounded-md bg-gradient-to-tr from-[#ac9276] to-[#d4c6bd] p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                  />
+                </div>
+              </div>
+            </div>
+
       </div>
     </>
   )
