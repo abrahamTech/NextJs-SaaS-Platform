ed3e7d14777187dc748f949b89b33c6ff63a41ec
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-01-11 00:52:59 -0600

    feat(Navbar): add pricing, login and register links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight } from "lucide-react";
 
 const Navbar = () => {
     return (
@@ -9,6 +12,22 @@ const Navbar = () => {
                     <Link href="/" className="flex z-40 font-semibold">
                         <span>pd(<span className="text-indigo-600">IA</span>)</span>
                     </Link>
+
+                    <div className="hidden items-center space-x-4 sm:flex">
+                        <>
+                            <Link href="/pricing" className={buttonVariants({size: "sm", variant: "ghost",})}>
+                                Pricing
+                            </Link>
+                            
+                            <LoginLink className={buttonVariants({size: "sm", variant: "ghost",})}>
+                                Sign In
+                            </LoginLink>
+
+                            <RegisterLink className={buttonVariants({size: "sm", className: "bg-indigo-500 hover:bg-indigo-500/90"})}>
+                                Get Started <ArrowRight className="ml-1 h-5 w-5"/>
+                            </RegisterLink>
+                        </>
+                    </div>
                 </div>
             </MaxWidthWrapper>
         </nav>
