0f2e668bcdb762eb6d83b4af0975b0575e405211
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-01-13 17:34:21 -0600

    feat(Dashboard Page): create basic dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,21 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+    const { getUser } = getKindeServerSession();
+
+    const user = await getUser()
+    console.log(user)
+
+    if(!user || !user.id) {
+        redirect("/auth-callback?origin=dashboard");
+    }
+
+    return (
+        <div>
+            Hello {user.given_name} {user.family_name}
+        </div>
+    )
+}
+
+export default Page;
