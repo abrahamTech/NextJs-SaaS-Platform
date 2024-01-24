acd7e38df92eacade4f93d83262936195232d43a
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-01-23 22:01:38 -0600

    feat(Dashboard[fileid]): prepare the div of the left sidebar and main wrapper

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -28,7 +28,15 @@ const Page = async ({params}: PageProps) => {
     if(!file) notFound()
 
     return (
-        <div>File View ID: {fileid}</div>
+        <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+            <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+                <div className="flex-1 xl:flex">
+                    <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+                        Left Sidebar
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
