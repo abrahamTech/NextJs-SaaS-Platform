9eb4091aa3877234b4d26d322259de3affa8f1df
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-01-25 21:34:11 -0600

    feat(UploadButton): install react-dropzone and add the component to drop the pdf file (UploadDropzone)

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -3,6 +3,23 @@
 import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
+import Dropzone from "react-dropzone";
+
+const UploadDropzone = () => {
+    return <Dropzone multiple={false}>
+        {({getRootProps, getInputProps, acceptedFiles}) => (
+            <div {...getRootProps()} className="border h-64 m-4 border-dashed border-gray-300 rounded-lg">
+                <div className="flex items-center justify-center h-full w-full">
+                    <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                            Example PDF
+                        </div>
+                    </label>
+                </div>
+            </div>
+        )}
+    </Dropzone>
+}
 
 const UploadButton = () => {
 
@@ -19,7 +36,7 @@ const UploadButton = () => {
             </DialogTrigger>
 
             <DialogContent>
-                Upload PDF
+                <UploadDropzone />
             </DialogContent>
         </Dialog>
     )
