7b5bb5697fe21bf76867f7ac5d9cda1098cb5ed0
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-02-01 22:15:20 -0600

    feat(UploadButton): implement progress bar with color change

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -118,7 +118,7 @@ const UploadDropzone = () => {
                             {isUploading
                                 ? (
                                     <div className="w-full mt-4 max-w-xs mx-auto">
-                                        <Progress value={uploadProgress} className="h-1 w-full bg-zinc-200" />
+                                        <Progress indicatorColor={uploadProgress === 100 ? "bg-green-500" : ""} value={uploadProgress} className="h-1 w-full bg-zinc-200" />
                                         {uploadProgress === 100 
                                             ? (
                                                 <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
