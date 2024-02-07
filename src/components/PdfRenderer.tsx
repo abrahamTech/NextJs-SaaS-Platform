3446cca797b30bcf958ee670677311e55584a7d5
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-02-06 21:41:49 -0600

    feat(PdfRender): implement functionality to page input button

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -11,6 +11,9 @@ import {useResizeDetector} from "react-resize-detector";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -27,7 +30,26 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
     const [numPages, setNumPages ] = useState<number>();
     const [currPage, setCurrPage] = useState<number>(1);
 
+    const CustomPageValidator = z.object({
+        page: z.string().refine((num) => Number(num) > 0 && Number(num) <= numPages!),
+    })
+
+    type TCustomPageValidator = z.infer<typeof CustomPageValidator>
+
+    const { register, handleSubmit, formState: { errors }, setValue } = useForm<TCustomPageValidator>({
+        defaultValues: {
+            page: "1",
+        },
+        resolver: zodResolver(CustomPageValidator)
+    });
+
     const { width, ref } = useResizeDetector();
+
+    const handlePageSubmit = ({ page, }: TCustomPageValidator) => {
+        setCurrPage(Number(page))
+        setValue("page", String(page))
+    }
+    
 
     return (
         <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
@@ -45,7 +67,15 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
                     </Button>
 
                     <div className="flex items-center gap-1.5">
-                        <Input className="w-12 h-8" />
+                        <Input 
+                            {...register("page")}
+                            className="w-12 h-8"
+                            onKeyDown={(e) => {
+                                if(e.key === "Enter") {
+                                    handleSubmit(handlePageSubmit)()
+                                }
+                            }}
+                        />
                         <p className="text-zinc-700 text-sm space-x-1">
                             <span>/</span>
                             <span>{numPages ?? "x"}</span>
