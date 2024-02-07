6c06a28fd88549e5482da5f80f93a937bdd1656c
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-02-06 21:54:30 -0600

    feat(PdfRender): show red border for invalid values on page input button

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -14,6 +14,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -69,7 +70,7 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
                     <div className="flex items-center gap-1.5">
                         <Input 
                             {...register("page")}
-                            className="w-12 h-8"
+                            className={cn("w-12 h-8", errors.page && "focus-visible:ring-red-500")}
                             onKeyDown={(e) => {
                                 if(e.key === "Enter") {
                                     handleSubmit(handlePageSubmit)()
