518ac4cf0067ee8745462b2408fa1590baaff21e
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-02-09 23:49:08 -0600

    fix(trpc index): use Query for getFileUploadStatus route

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -1,7 +1,17 @@
+import { trpc } from "@/app/_trpc/client";
 import ChatInput from "./ChatInput";
 import Messages from "./Messages";
 
-const ChatWrapper = () => {
+interface ChatWrapperProps {
+    fileId: string
+}
+
+const ChatWrapper = ({fileId}: ChatWrapperProps) => {
+
+    const {} = trpc.getFileUploadStatus.useQuery({
+        fileId,
+    })
+
     return (
         <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
             <div className="flex-1 justify-between flex flex-col mb-28">
