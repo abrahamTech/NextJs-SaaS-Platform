723605f7a965801ab3b7bef1f5a48b07c81c3e4b
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-02-13 21:24:41 -0600

    feat(ChatContext): add ChatContext initial structure

---
+++ src/components/chat/ChatContext.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode, createContext } from "react";
+
+type StreamResponse = {
+    addMessage: () => void,
+    message: string,
+    handleInputChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void,
+    isLoading: boolean,
+}
+
+export const ChatContext =  createContext({
+    addMessage: () => {},
+    message: '',
+    handleInputChange: () => {},
+    isLoading: false,
+})
+
+
+interface Props {
+    fileId: string,
+    children: ReactNode
+}
+
+export const ChatContextProvider = ({fileId, children}: Props) => {
+    
+}
