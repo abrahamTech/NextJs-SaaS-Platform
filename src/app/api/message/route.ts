a7e26d64e01131c67d482d5343447dac5d7e44bd
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-02-14 22:45:02 -0600

    feat(message route): create the message with the necessary information

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -28,4 +28,12 @@ export const POST = async (req: NextRequest) => {
     if(!file)
         return new Response("Not found", {status: 404})
 
+    await db.message.create({
+        data: {
+            text: message,
+            isUserMessage: true,
+            userId,
+            fileId,
+        },
+    })
 }
